6309019e97b6ca6c629e73f47a8fbe2f7a64c772
Ben Ferguson <bent@Bens-MacBook-Air.local>
2022-02-08 22:10:07 -0500

    refactor cleanup

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <!--GameCard /-->
-    <Index />
+    <GameCard />
+    <!--Index /-->
   </div>
 </template>
 
 <script>
-import Index from "/src/components/Index.vue"
-//import GameCard from './components/Game.vue'
+//import Index from "/src/components/Index.vue"
+import GameCard from '/src/components/Game.vue'
 
 export default {
   name: 'App',
   components: {
-    //GameCard,
-    Index
+    GameCard,
+    //Index
   }
 }
 </script>
